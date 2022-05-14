5950ce7e698d00aace80d72b7d6dd2c8881a4bc8
Mmaz19 <matteo@iMac-di-Matteo.home>
2022-05-14 21:27:48 +0200

    finished UI

--- app.js
+++ app.js
@@ -12,36 +12,62 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection == 'scissors' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'rock')){
             result = (`You win! ${playerSelection} beats ${computerSelection} `)
+            resultDiv.innerText = result;
+            resultDiv.classList.remove('tie', 'loser');
+            resultDiv.classList.add('winner');
             playerScore++;
         } else if (playerSelection === computerSelection){
             result = (`Tie!`)
+            resultDiv.innerText = result;
+            resultDiv.classList.remove('winner', 'loser');
+            resultDiv.classList.add('tie');
+
         }else {
             result = (`You lost! ${computerSelection} beats  ${playerSelection}`)
+            resultDiv.innerText = result;
+            resultDiv.classList.remove('tie', 'winner');
+            resultDiv.classList.add('loser');
+            
             computerScore++;
         }
+       
         console.log(result);
         return 
   }
 
-  function game(){
-      
-      for( let i = 0; i<5; i++){
-        let playerSelection = prompt('Choose Wisely').toLowerCase();
+  function game(playerSelection){
         let computerSelection = computerPlay();
         playRound(playerSelection,computerSelection);
-        console.log(playerScore, computerScore);
+        roundCount++;
+        if (roundCount == 5){
+        gameOver.style.display = 'block';
+        if (computerScore > playerScore){
+            return showFinal.innerText = `You Lost! Computer: ${computerScore} Player:${playerScore}`
+        }else if (playerScore > computerScore){
+          return showFinal.innerText = `You Win!  Player:${playerScore} Computer: ${computerScore}`
+      }else{
+          return showFinal.innerText = `Tie!  Player:${playerScore} Computer: ${computerScore}`
       }
-      if (computerScore > playerScore){
-          return console.log(`You Lost! Computer: ${computerScore} Player:${playerScore}`)
-      }else if (playerScore > computerScore){
-        return console.log(`You Win!  Player:${playerScore} Computer: ${computerScore}`)
-    }else{
-        return console.log(`Tie!  Player:${playerScore} Computer: ${computerScore}`)
-    }
+        }else{
+        
+        counting.innerText = roundCount;
+        scoreNum.innerText = playerScore;
+        }
+      
+      
       
   }
 
   //counter
+  let resultDiv = document.querySelector('.results');
+  let body = document.querySelector('body');
   let playerScore = 0;
   let computerScore = 0;
-  game();
+  let playerSelection = document.addEventListener('click', function(e){
+      return game(e.path[1].id);
+  } );
+  let counting = document.querySelector('.counterUp');
+  let roundCount= 0;
+  let gameOver = document.querySelector('.gameOver');
+  let scoreNum = document.querySelector('.playerScoreNumber');
+  let showFinal = document.querySelector('.showFinal');
